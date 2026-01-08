3f7d715f5ef84140e984ca1e0fab46dc091cf357
vietanhdang <vietdang0407@gmail.com>
2026-01-08 23:05:57 +0700

    feat: remove TodoService from main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
-import { TodoService } from './app/services/todo.service';
 
 bootstrapApplication(AppComponent, {
 	providers: [
 		provideHttpClient(),
-		TodoService
 	]
 }).catch(err => console.error(err));
 
