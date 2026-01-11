b800a8b84b2d5574f2291281e5dfff64af995335
vietanhdang <vietdang0407@gmail.com>
2026-01-11 22:57:53 +0700

    feat: enhance ClickOutsideDirective with initialization logic to improve event handling

--- src/app/directives/click-outside.directive.ts
+++ src/app/directives/click-outside.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 
 /**
  * Custom Directive: appClickOutside
@@ -13,13 +13,26 @@ import { Directive, ElementRef, EventEmitter, HostListener, Output } from '@angu
   selector: '[appClickOutside]',
   standalone: true,
 })
-export class ClickOutsideDirective {
+export class ClickOutsideDirective implements OnInit {
   @Output() clickOutside = new EventEmitter<void>();
+  private isInitialized = false;
 
   constructor(private el: ElementRef) {}
 
+  ngOnInit() {
+    // Đợi một chút để bỏ qua click event đang bubble từ element trigger directive
+    setTimeout(() => {
+      this.isInitialized = true;
+    }, 0);
+  }
+
   @HostListener('document:click', ['$event.target'])
   onClick(target: HTMLElement) {
+    // Bỏ qua click đầu tiên ngay sau khi directive được khởi tạo
+    if (!this.isInitialized) {
+      return;
+    }
+
     const clickedInside = this.el.nativeElement.contains(target);
     if (!clickedInside) {
       this.clickOutside.emit();
